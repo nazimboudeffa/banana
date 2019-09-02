2e1ae43b22bf552a0f3a5c0d45e6ee90d197a96e
Nazim Boudeffa <boudeffa@gmail.com>
2019-09-02 04:49:22 +0200

    First Commit

--- build/all.js
+++ build/all.js
@@ -17,10 +17,13 @@
 * @license      {@link https://github.com/nazimboudeffa/banana/blob/master/license.txt|MIT License}
 */
 
-Banana.Application = function(){
-
+Banana.Application = function(config){
+  this.target = config.target || document.createElement('div');
 };
 
 Banana.Application.prototype = {
-
+  run: function(){
+    var textNode = document.createTextNode("Basic Example");
+		document.getElementById(this.target).appendChild(textNode);
+  }
 };
---
+++ src/control/Page.js
@@ -0,0 +1,15 @@
+/**
+* @author       Nazim Boudeffa <boudeffa@gmail.com>
+* @copyright    2019 Nazim Boudeffa.
+* @license      {@link https://github.com/nazimboudeffa/banana/blob/master/license.txt|MIT License}
+*/
+
+Banana.Page = function(){
+
+};
+
+Banana.Page.prototype = {
+  addControl: function(){
+
+  }
+};
